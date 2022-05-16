bf954ad3c1d52c0ad43ebf6cf704c27e692d0d41
unknown <Bawiani>
2022-05-16 11:27:23 +0000

    Changes to member

--- routers/member.js
+++ routers/member.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = new express.Router();
 
-router.get('/newmember', (req, res) => {
-    res.render('newmember', {
-        layout: 'index'
-    });
-});
+// router.get('/newmember', (req, res) => {
+//     res.render('newmember', {
+//         layout: 'index'
+//     });
+// });
 
 router.get('/register', (req, res) => {
     res.render('register', {
